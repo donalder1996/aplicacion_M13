fa4306f61f65d7ff7d8b724d56b3600ff608d7a8
Jonathan <jonigg9@gmail.com>
2023-11-02 17:46:07 +0100

    Primer Activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.aplicacion_m13"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.aplicacion_m13"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/aplicacion_m13/MainActivity.java
+++ app/src/main/java/com/example/aplicacion_m13/MainActivity.java
@@ -3,12 +3,49 @@ package com.example.aplicacion_m13;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText etUsuario, etContraseña;
+    private Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etUsuario = findViewById(R.id.editTextNombre);
+        etContraseña = findViewById(R.id.editTextContra);
+        btn1 = findViewById(R.id.button);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                iniciarActividad();
+
+            }
+
+        });
     }
+
+        private void iniciarActividad(){
+            String usuario = etUsuario.getText().toString();
+            String contrasena = etContraseña.getText().toString();
+            //Si la opción primera o segunda está vacío mostrar un mensaje mediante toast
+            if (usuario.isEmpty() && contrasena.isEmpty()) {
+                Toast.makeText(this, "campos en blanco", Toast.LENGTH_SHORT).show();
+
+            }
+            if (usuario.isEmpty()) {
+                Toast.makeText(this, "usuario en blanco", Toast.LENGTH_SHORT).show();
+            }
+            if (contrasena.isEmpty()) {
+                Toast.makeText(this, "contraseña en blanco", Toast.LENGTH_SHORT).show();
+
+
+            }
+
+        }
+
 }
