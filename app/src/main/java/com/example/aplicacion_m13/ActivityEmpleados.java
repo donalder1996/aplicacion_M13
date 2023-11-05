68e457d137ee2cf01c882905d0a5838447a060ed
Jonathan <jonigg9@gmail.com>
2023-11-05 17:04:47 +0100

    Activitys acabado

--- app/src/main/java/com/example/aplicacion_m13/ActivityEmpleados.java
+++ app/src/main/java/com/example/aplicacion_m13/ActivityEmpleados.java
@@ -1,14 +1,30 @@
 package com.example.aplicacion_m13;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ActivityEmpleados extends AppCompatActivity {
+    private TextView tvNombre, tvDatos;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empleados);
+        tvNombre = findViewById(R.id.textNombre);
+        tvDatos = findViewById(R.id.textNombre);
+        //Para añadir la información de la base de datos
+        //tvNombre.setText(prueba);
+        //tvDatos.setText();
     }
+
+
+
 }
--- app/src/main/java/com/example/aplicacion_m13/ActivityEmpleadosEditar.java
+++ app/src/main/java/com/example/aplicacion_m13/ActivityEmpleadosEditar.java
@@ -1,13 +1,55 @@
 package com.example.aplicacion_m13;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ActivityEmpleadosEditar extends AppCompatActivity {
-
+    private Button btn;
+    private TextView tvNombre, tvDatos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empleados_editar);
+        tvNombre = findViewById(R.id.textNombre);
+        tvDatos = findViewById(R.id.textNombre);
+        //Para añadir la información de la base de datos
+        //tvNombre.setText(prueba);
+        //tvDatos.setText();
+        btn = findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarDialogo();
+            }
+        });
+    }
+    //Este alerDialog permite al usuario la opción de mantenerse en la pantalla actual o pasar al listado
+    private void mostrarDialogo() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Selecciona una opción")
+                .setMessage("¿Quieres permanecer en esta pantalla o ir al listado de empleados")
+                .setPositiveButton("Permanecer", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Acción a realizar al seleccionar "Permanecer"
+                        dialog.dismiss(); // Cierra el diálogo
+                    }
+                })
+                .setNegativeButton("Ir a listado", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Acción a realizar al seleccionar "Ir a la siguiente pantalla
+                        Intent intent = new Intent((getApplication()),EmpleadosLista.class);
+                        startActivity(intent);
+                    }
+                })
+                .show();
     }
 }
